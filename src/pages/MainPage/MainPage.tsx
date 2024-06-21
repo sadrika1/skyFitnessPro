c72a8f59c7361ec54f4f625b99ede92e3a09be46
«Annavtropikah» <kozhevnikova.it@gmail.com>
2024-06-21 23:10:56 +0300

    tilewind

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -5,13 +5,13 @@ export default function MainPage() {
     return (
         <>
             <div>
-                <h1>Начните заниматься спортом и улучшите качество жизни</h1>
-                <div>
+                <h1 >Начните заниматься спортом и улучшите качество жизни</h1>
+                <div className=" bg-yellow-100 ">
                     <h6>Измени своё тело за полгода!</h6>
                 </div>
                 <Courses />
             
-                <button>Наверх ↑</button>
+                <button className=" bg-yellow-400 ">Наверх ↑</button>
             </div>
         </>
 
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.{html,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// module.exports= {
+//   content: ["./src/*.tsx"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
