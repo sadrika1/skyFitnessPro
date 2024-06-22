0a7123adddc6f93c94630754677ebc79ea054af0
Elena Bersh <145923442+lenaber6@users.noreply.github.com>
2024-06-22 10:32:08 +0300

    попытка добавить кнопку 1

---
+++ skyFitnessPro-dev/skyFitnessPro-dev/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import ChosenCoursePage from "./pages/ChosenCoursePage";
+
+function App() {
+  return( 
+  <>
+  <h1>hey!</h1>
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Click me
+</button>
+  <ChosenCoursePage />
+  </>
+  )
+}
+
+export default App;
