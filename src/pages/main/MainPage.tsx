f20d9b18de48d12c06818afc1e626a711dc84385
«Annavtropikah» <kozhevnikova.it@gmail.com>
2024-07-02 20:07:34 +0300

    цвета

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -21,7 +21,7 @@ export default function MainPage() {
                 <div className="mx-[140px] max-w-[1440px]">
                     <div className="flex justify-between my-[50px] relative">
                         <div className="font-semibold text-[60px] h-[120px] text-pretty inline-block align-middle" >Начните заниматься спортом и улучшите качество жизни</div>
-                        <div className="mt-6  p-4 text-[32px] bg-green  min-w-[288px] max-h-[125px] rounded-md ">Измени своё тело за полгода!</div>
+                        <div className="mt-6  p-4 text-[32px] bg-custom-green  min-w-[288px] max-h-[125px] rounded-md ">Измени своё тело за полгода!</div>
                         <div className="absolute top-[134px] right-[150px]">
                             <img src="images/polygon.png" alt="polygon" />
                         </div>
@@ -41,7 +41,7 @@ export default function MainPage() {
 
 
                     <div className="flex justify-center ">
-                        <button className="mt-8 mb-[80px] rounded-3xl bg-green w-[127px] h-[52px] ">Наверх ↑</button>
+                        <button className="mt-8 mb-[80px] rounded-3xl bg-custom-green w-[127px] h-[52px] ">Наверх ↑</button>
                     </div>
                 </div>
             </div>
