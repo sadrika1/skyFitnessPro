78efe3d62f7fdf2a718911fefbe07c504b6058a0
annavtropikah <146850791+annavtropikah@users.noreply.github.com>
2024-07-03 21:16:55 +0300

    Revert "Main favourite"

--- src/route/protectedRoute.tsx
+++
@@ -1,7 +0,0 @@
-import { getAuth } from "firebase/auth";
-import { Navigate, Outlet } from "react-router-dom";
-
-export default function ProtectedRoute() {
-  const auth = getAuth();
-  return auth ? <Outlet /> : <Navigate to="/" />;
-}
