2e7e99fe7bc88b29ee086fe305edb4e978a9aaa4
lenaber6 <lenaber6@mail.ru>
2024-07-04 12:56:07 +0300

    почти пустой App.css, сделала компонент SkillCard

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,12 +4,12 @@ import LoginBtn from "../button/LoginBtn";
 export default function Header() {
   return (
     // указать проверку авторизации и условие
-    <div className="flex justify-between center items-center">
+    <div className="flex justify-between px-[30px] lg:px-[140px] py-[16px] lg:py-[50px] items-center">
       <div>
         <Link to="/">
-          <img src="../public/images/logo.svg" alt="Logo" />
+          <img src="/images/logo.svg" alt="Logo" />
         </Link>
-        <p className="laptop">Онлайн-тренировки для занятий дома</p>
+        <p className="sm:hidden lg:flex font-roboto">Онлайн-тренировки для занятий дома</p>
       </div>
       <LoginBtn />
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,33 +9,28 @@ module.exports = {
       },
       height: {
         h639px: "639px",
-        "h-7-2": "1.97rem" /* 30px */,
-        "h-15": "3.25rem" /* 52px */,
-        "h-35": "8.8rem" /* 141px */,
-        "h-37": "9.1rem" /* 146px */,
-        "h-79": "19.38rem" /* 310px */,
-        "h-100": "25.7rem" /* 412px */,
-        "h-103": "30.3rem" /* 486px */,
+        // "h-7-2": "1.97rem" /* 30px */,
+        // "h-15": "3.25rem" /* 52px */,
+        // "h-37": "9.1rem" /* 146px */,
+        // "h-100": "25.7rem" /* 412px */,
+        // "h-103": "30.3rem" /* 486px */,
       },
       width: {
-        "w-7-2": "1.97rem" /* 30px */,
-        "w-84": "23rem" /* 368px */,
-        "w-88": "26.75rem" /* 431px */,
-        container: "22rem" /*353px*/,
+        // "w-7-2": "1.97rem" /* 30px */,
       },
       rounded: {
         radiusVideo: "30px",
       },
 
       padding: {
-        "p-8-2": "1.97rem" /*30px*/,
-        "pt-15": "3.75rem" /* 60px */,
-        "pt-26": "6.37rem" /* 102px */,
-        "pt-38": "9.75rem" /* 156px */,
+        // "p-8-2": "1.97rem" /*30px*/,
+        // "pt-15": "3.75rem" /* 60px */,
+        // "pt-26": "6.37rem" /* 102px */,
+        // "pt-38": "9.75rem" /* 156px */,
       },
       screens: {
-        sm: "425px",
-        // => @media (min-width: 425px) { ... }
+        sm: "375px",
+        // => @media (min-width: 375px) { ... }
 
         md: "768px",
         // => @media (min-width: 768px) { ... }
@@ -44,22 +39,24 @@ module.exports = {
         // => @media (min-width: 1440px) { ... }
       },
       gap: {
-        "gap-17": "1.06rem" /*17px */,
-        "gap-8-1": "2.1rem" /* 34px */,
-        "gap-31": "7.8rem" /* 124px */,
+        // "gap-8-1": "2.1rem" /* 34px */,
+        // "gap-31": "7.8rem" /* 124px */,
       },
       fontSize: {
-        "7.2xl": "4.8rem",
-        "text-3-1xl": "1.975rem" /* 32px */,
+        // "text-3-1xl": "1.975rem" /* 32px */,
+        
       },
+      // fontWeight: {
+      //   'font-medium': "500",
+      // },
       lineHeight: {
-        "leading-4-1": "1.03rem" /* 17,6px */,
-        "leading-6-1": "1.6rem" /* 26,4px */,
-        "leading-8-9": "2.19rem" /* 35,2px */,
-        "leading-11": "2.75rem" /*44px*/,
-        "leading-15": "3.75rem" /*60px */,
-        "leading-16": "4rem" /*64px */,
-        "leading-26": "6.3rem" /*101.25 */,
+        // "leading-4-1": "1.03rem" /* 17,6px */,
+        // "leading-6-1": "1.6rem" /* 26,4px */,
+        // "leading-8-9": "2.19rem" /* 35,2px */,
+        // "leading-11": "2.75rem" /*44px*/,
+        // "leading-15": "3.75rem" /*60px */,
+        // "leading-16": "4rem" /*64px */,
+        // "leading-26": "6.3rem" /*101.25px */,
       },
       colors: {
         "custom-yellow": "#FFC700",
